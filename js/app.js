96030f9ae52128955f2d329c9c8fc172542bf4ef
Stephen Axtell <axtell.stephen@gmail.com>
2020-07-02 11:53:30 +0200

    reading and displaying data dynamically as well as saving to localstorage

--- js/app.js
+++ js/app.js
@@ -1,50 +1,81 @@
 // variables of selected elements
 
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.products-center');
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
 
 // cart
 let cart = [];
 
 // getting products
 class Products {
-		async getProducts() {
-				try {
-						let result = await fetch('products.json');
-						let data = await result.json();
-						let products = data.items;
-						products = products.map(item => {
-								const {title, price} = item.fields;
-								const {id} = item.sys;
-								const image = item.fields.image.fields.file.url;
-								return {title, price, id, image}
-						})
-						return products;
-				} catch (error) {
-						console.log(error);
-				}
-		}
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 // displaying the products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+			<!-- single product -->
+			<article class="product">
+				<div class="img-container">
+					<img
+						src=${product.image}
+						alt="Product"
+						class="product-img"
+					/>
+					<button class="bag-btn" data-id=${product.id} title="add to bag">
+						<i class="fas fa-shopping-cart"></i>
+						add to bag
+					</button>
+				</div>
+				<h3>${product.title}</h3>
+				<h4>$${product.price}</h4>
+			</article>
+			<!-- single product ends -->
+			`;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
 
 // Working with local storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-		const ui = new UI();
-		const products = new Products();
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
 
-		// get all products
-		products
-				.getProducts()
-				.then(data => console.log(data));
+  // get all products
+  products.getProducts().then((products) => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products); // can do it like this because its a static method
+  });
 });
