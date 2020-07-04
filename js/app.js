69390f6c8788d251d28b8a5e9711ba52f6b92156
Stephen Axtell <axtell.stephen@gmail.com>
2020-07-04 11:47:37 +0200

    various functions to setup the app, includes adding some eventlisteners

--- js/app.js
+++ js/app.js
@@ -150,6 +150,27 @@ class UI {
       .classList
       .add('showCart');
   }
+
+  setupApp() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener('click', this.showCart);
+    closeCartBtn.addEventListener('click', this.hideCart);
+  }
+
+  populateCart(cart) {
+    cart.forEach(item => this.addCartItem(item));
+  }
+
+  hideCart() {
+    cartOverlay
+      .classList
+      .remove('transparentBcg');
+    cartDOM
+      .classList
+      .remove('showCart');
+  }
 }
 
 // Working with local storage
@@ -166,11 +187,20 @@ class Storage {
   static saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+
+  static getCart() {
+    return localStorage.getItem('cart')
+      ? JSON.parse(localStorage.getItem('cart'))
+      : [];
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+
+  // setup application
+  ui.setupApp()
 
   // get all products
   products
